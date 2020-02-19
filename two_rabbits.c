da91cc1916ed9e0e417743cfd40678ea6133d461
Nilotpal Mrinal <nilotpal.avanti@gmail.com>
2020-02-19 08:46:54 +0530

    Create two_rabbits.c

---
+++ two_rabbits.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+  int t,i;
+   scanf("%d", &t);
+
+for(i=0;i<t;i++)
+{   
+   long long int a_loc,b_loc,ajmp,bjmp;
+
+   scanf("%lld%lld%lld%lld", &a_loc, &b_loc, &ajmp, &bjmp);
+
+
+
+
+
+
+
+}
+
+
+
+}
