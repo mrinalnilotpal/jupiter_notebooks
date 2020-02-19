a2368704e0d5514ff4473f0016151ac434390f92
Nilotpal Mrinal <nilotpal.avanti@gmail.com>
2020-02-19 09:23:19 +0530

    Update two_rabbits.c

--- two_rabbits.c
+++ two_rabbits.c
@@ -7,18 +7,30 @@ int main()
 
 for(i=0;i<t;i++)
 {   
-   long long int a_loc,b_loc,ajmp,bjmp;
+   long long int a_loc,b_loc,ajmp,bjmp, count=0;
 
    scanf("%lld%lld%lld%lld", &a_loc, &b_loc, &ajmp, &bjmp);
 
+  while(a_loc <= b_loc)
+{
+    if(a_loc = b_loc)
+      {
+           printf("%lld", count);
+           break;
 
+      }
 
+     else if (a_loc > b_loc)
+         {
+           printf("-1");
+         }
+  else {
+         a_loc = a_loc + ajmp;
+         b_loc = b_loc - bjmp;
 
+        count = count + 1;
 
-
-
+       }
 }
-
-
-
+}
 }
